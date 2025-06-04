eaf79159d86a7a40597750a6869c408c786dbb8c
Venkatesan Mahendran <venkatesan.mahendran@gmail.com>
2025-06-04 17:48:07 +0530

    Chapter 5 to Chapter 9 changes

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -24,6 +24,10 @@ export const formatDateToLocal = (
 export const generateYAxis = (revenue: Revenue[]) => {
   // Calculate what labels we need to display on the y-axis
   // based on highest record and in 1000s
+
+  console.warn("Inside generateYAxis");
+  // console.warn(revenue);
+
   const yAxisLabels = [];
   const highestRecord = Math.max(...revenue.map((month) => month.revenue));
   const topLabel = Math.ceil(highestRecord / 1000) * 1000;
